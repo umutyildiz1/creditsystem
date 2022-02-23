f15bd49eebbd4eb703808b225e556d8490658540
umutyildiz <56760679+umutyildiz1@users.noreply.github.com>
2022-02-23 21:26:34 +0300

    Security implementation with JWT is achieved

--- creditsystem/src/main/java/com/paycoreumutyildiz/creditsystem/Repository/CustomerRepository.java
+++ creditsystem/src/main/java/com/paycoreumutyildiz/creditsystem/Repository/CustomerRepository.java
@@ -3,9 +3,10 @@ package com.paycoreumutyildiz.creditsystem.Repository;
 import com.paycoreumutyildiz.creditsystem.Model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 
-
+@Repository
 public interface CustomerRepository extends JpaRepository<Customer,Long> {
     @Query("select c from Customer c where c.phoneNumber =:phoneNumber")
     Customer findByPhoneNumber(String phoneNumber);
